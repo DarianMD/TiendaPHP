0f977f8ab8564f8514e29f78cfee045bb02cbb55
DarianMD <darianmanasturean@gmail.com>
2021-10-25 20:10:07 +0200

    session

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php
-
   include ('/controllers/db.php');
-
   $producto="SELECT * from producto";
-
   $result = $conexion->query($producto);
+
 
 ?>
 
@@ -20,17 +18,36 @@
 </head>
 <body>
 
-  <div class="nav">
-      <p>DarianShop</p>
-      <a href="register.html">Registro</a>
-      <a href="login.php">Login</a>
-      <p> <?php echo "Usuario X" ?> </p>
-  </div>
 
 
-    <div class="main-carousel" data-flickity='{ "cellAlign": "left", "contain": true }'>
 
+<nav class="navigation-wrapper">
+  <ul class="nav-list">
+    <li class="nav-item">
+      <a href="#/"><em>Home</em></a>
+    </li>
     
+    <li class="nav-item">
+      <?php
+        if(session_id() == ''){
+          echo "<a href='login.php'><em>Login</em></a>";
+
+        }
+       else{
+         echo "Usuario X";
+         echo "<br>";
+         echo "<a href='logout.php'><em>Logout</em></a>";
+
+       }
+      
+      ?>
+    </li>
+ 
+  </ul>
+</nav>
+
+  <div class="productos">
+  <div class="main-carousel" data-flickity='{ "cellAlign": "left","freeScroll": true ,"wrapAround": true, "contain": true}'>
       <?php 
         
          if($result->num_rows > 0){
@@ -39,7 +56,7 @@
             <h1>".$row["nom_product"]."</h1>
             <p>".$row["desc_product"]."</p>
             <p>".$row["precio"]."</p>
-            <a href='logica.php?id=".$row["id"]."'>Login</a>
+            <a href='logica.php?id=".$row["id"]."'>Comprar</a>
             <br>
             </div>";
             
@@ -48,6 +65,8 @@
      ?>
     </div>
   
+  </div>
+    
 
     <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
     <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
